3cce42218b3b2b75433bb5d0942b871322b705a3
Eavlee <422628422@qq.com>
2020-01-21 17:26:37 +0800

    dist

---
+++ dist/precache-manifest.347f94f6187e83e4bffdb88f4a4ff642.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f0ba4ec8196726ba33",
+    "url": "/css/app.485f6fe7.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "347198e955b9a2c6ca468a068a5df2e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "775f535ac31e1563ef0c",
+    "url": "/js/about.7c0c9c53.js"
+  },
+  {
+    "revision": "d0f0ba4ec8196726ba33",
+    "url": "/js/app.f3b57b18.js"
+  },
+  {
+    "revision": "6f93fb2e5f18372932d9",
+    "url": "/js/chunk-vendors.b42a6c66.js"
+  },
+  {
+    "revision": "7d065064b1508ff4047c7d8b4fd98709",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
